4024e7d555cd6861115225adb01e6a68ec96ae96
Mkrishna <mkrishnamoorthy91@gmail.com>
2026-01-31 19:10:06 +0530

    Add multi-cloud Azure, AWS networking Terraform configuration

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "environment" {
+	type = string
+	description = "Environment."
+}
+
+variable "azure_subscription_id" {
+  type = string
+  description = "Azure subscription identifier."
+}
